1ed55cc9af2c6a5ccc8df64d11e5da31e67647b8
Iniyan88 <biniyan2004@gmail.com>
2024-03-20 10:21:25 +0530

    final

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient;
-}
+const prisma = new PrismaClient();
 
-if (process.env.NODE_ENV === "production") {
-  global.prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-}
-
-export default global.prisma;
+export default prisma;
